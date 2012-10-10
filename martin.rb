6e80e620b2f2b808783ebf4ec3f5ddcffe798882
Martin Rados <radosm@gmail.com>
2012-10-10 17:56:22 -0300

    pruebas de la catedra

--- martin.rb
+++ martin.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 require 'set'
 
 class String
@@ -170,32 +172,79 @@ end
 raices = ["bolud","pelotud","put","forr","pollerud","mequetref"]
 
 literal = CategoriaSufijos.new raices, Set["o", "a", "e"]
-flexionado = CategoriaSufijos.new raices, Set["os", "as"]
-diminutivo = CategoriaSufijos.new raices, Set["ito", "ita"]
-aumentativo = CategoriaSufijos.new raices, Set["ote", "ota", "azo", "aza", "on", "ona"]
+flexionado = CategoriaSufijos.new raices, Set["os", "as", "es"]
+diminutivo = CategoriaSufijos.new raices, Set["ito", "ita", "itos", "itas", "in", "ines" ]
+aumentativo = CategoriaSufijos.new raices, Set["ote", "ota", "azo", "aza", "on", "ona", "isima", "isimo"]
+peyorativo = CategoriaSufijos.new raices, Set[ "oncho", "oncha", "onchos", "onchas", "ongo", "ongos", "onga", "ongas" ]
+prefijo = CategoriaPrefijos.new raices, Set[ "hiper", "super", "re", "remil" ]
 
 # Crea Moderador
 
-buscador = BuscadorDeEvidencia.new Set[literal, flexionado, diminutivo, aumentativo]
+buscador = BuscadorDeEvidencia.new Set[literal, flexionado, diminutivo, aumentativo, prefijo, peyorativo]
 
-simbolosPorLetras = ReemplazarTexto.new Hash['0'=>'o','1'=>'l','4'=>'a','3'=>'e','5'=>'s','2'=>'z', '6'=>'g', '9'=>'q']
+simbolosPorLetras = ReemplazarTexto.new Hash['0'=>'o','1'=>'l','4'=>'a','3'=>'e','5'=>'s','2'=>'dos', '6'=>'g', 
+                                             'á'=>'a','é'=>'e','í'=>'i','ó'=>'o','ú'=>'u','7'=>'t']
 fonetico = ReemplazarTexto.new Hash['ah'=>'a','eh'=>'e','ih'=>'i','oh'=>'o','uh'=>'u']
 filtrador = FiltradorDeTexto.new Set[simbolosPorLetras, EliminarSeparaciones.new, EliminarRepeticiones.new ,fonetico]
 moderador = Moderador.new buscador, filtrador, AnalizadorBasico
 
-# Pruebas
+# Pruebas nuestras
 
-[ 
-  "hola, como andas?", 
-  "hola boludo, como andas? h i j o d e p/u u u t a",
-  "hola b0ludo, como andas?",
-  "hola b 0 ludo, como andas?",
-  "hola puutooo, como andas?",
-  "hola put0 o, como andas?",
-  "hola FORRO, como andas?",
-  "hola mequ3e3hhhhtrrrefe, como andas?",
-  "hola pollehhehhrudooooo, como andas?",
-  "hola foro, como andas? sos un put42o !"
+##[ 
+  ##"hola, como andas?", 
+  ##"hola boludo, como andas? h i j o d e p/u u u t a",
+  ##"hola b0ludo, como andas?",
+  ##"hola b 0 ludo, como andas?",
+  ##"hola puutooo, como andas?",
+  ##"hola pu t 0 o, como andas?",
+  ##"hola FORRO, como andas?",
+  ##"hola mequ3e3hhhhtrrrefe, como andas?",
+  ##"hola pollehhehhrudooooo, como andas?",
+  ##"hola foro, como andas? sos un put42o !"
+##].each do |comentario|
+	##resultado=moderador.analizarComentario(comentario)
+	##print comentario+": "+ resultado.publicable?.to_s+"  -  Insultos: "+(resultado.insultos.to_a).to_s+"\n"
+##end 
+
+# Pruebas de la catedra
+
+[
+	"¿Por qué el pollo cruzó el camino?",
+	"El pollo este es un pelotudo.",
+	"El muy hijo de puta cruzó el camino cuando nadie lo miraba.",
+	"Los niños y las niñas son los únicos y las únicas privilegiados y privelegiadas.",
+	"Pelotudos y boludas son iguales ante la ley.",
+	"Los muy hijos de puta se fueron sin avisarme.",
+	"Te mando un besito grande.",
+	"¿Quién va a ser, si no el pelotudito de siempre?",
+	"¡Tremendo boludín viniste a salir!",
+	"De golpe y porrazo me moría de hambre.",
+	"Forrazo como no hay dos.",
+	"La putísima madre que lo parió.",
+	"Libracos no, alpargatuchas sí.",
+	"A pelotudoncho no te gana nadie.",
+	"El rey de los boludongos.",
+	"Hipermercado supercalifragilisticoexpialidoso.",
+	"¡Hijo de la remilputa!",
+	"Superboludo al rescate.",
+	"E-s-p-e-c-t-a-c-u-l-a-r",
+	"Se la dió de p.e.l.o.t.u.d.o.",
+	"Le pasó x h i j o d e p u t a.",
+	"Parece verdadero pelo tu disfraz de gorila.",
+	"No te diste cuenta por pelo-tudo.",
+	"No te diste cuenta por bo lu do.",
+	"Diputados tratan prohibición de llamar ciencia a la computación.",
+	"¿Qué mirás, pelotudo.com?",
+	"Sólo un hijodeputa puede querer algo así.",
+	"L33t a1N7 p-r8 NOR 1am3.",
+	"Hay que explicárselo al p3l07udo de siempre.",
+	"A los b01u2 hay decírselos dos veces.",
+	"Amerika perdida.",
+	"¿Es voludo o se hace?",
+	"El ijoeputa me dejó de garpe.",
+	"El campana: es taaan-tonn-tiiiíín...",
+	"Andaaaaáááá, boludoooo.",
+	"Rajá, boluuuuudo."
 ].each do |comentario|
 	resultado=moderador.analizarComentario(comentario)
 	print comentario+": "+ resultado.publicable?.to_s+"  -  Insultos: "+(resultado.insultos.to_a).to_s+"\n"
